1f711aeac6afb6e34d62e5d15e0ef64356287736
SofiaShvedova <90041135+SofiaShvedova@users.noreply.github.com>
2021-11-22 19:01:46 +0300

    1

--- Практическая_работа_5.py
+++ Практическая_работа_5.py
@@ -1,1 +1,16 @@
 
+class Win_Door:
+  def __init__(self,x,y):
+    self.square = x*y
+
+class Room: 
+  def __init__(self,x,y,z):
+    self.square = 2*z*(x+y)
+    self.wd = []
+  def addWD(self,w,h):
+    self.wd.append(Win_Door(w,h))
+  def workSurface(self):
+    new_square = self.square
+    for i in self.wd:
+      new_square -=i.square
+    return new_square
